7a0e51acf6a56aa7405a7fa556b79846554344d8
akhil <akhiltiwari.13@gmail.com>
2025-09-26 13:46:16 +0530

    minor changes to general workflow

--- lua/cocoed/plugins/general_workflow.lua
+++ lua/cocoed/plugins/general_workflow.lua
@@ -11,21 +11,21 @@ return {
     'obsidian-nvim/obsidian.nvim',
     version = 'v3.12.0', -- recommended, use latest release instead of latest commit
     lazy = true,
-    -- ft = "markdown",
+    ft = 'markdown',
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    event = {
-      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-      -- refer to `:h file-pattern` for more examples
-      'BufReadPre '
-        .. vim.fn.expand '~'
-        .. '/files/notes/*/*.md',
-      'BufNewFile ' .. vim.fn.expand '~' .. '/files/notes/*/*.md',
-      -- 'BufReadPre '
-      --   .. vim.fn.expand '~'
-      --   .. 'files/notes/*.md',
-      -- 'BufNewFile' .. vim.fn.expand '~' .. 'files/notes/*.md',
-    },
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   'BufReadPre '
+    --     .. vim.fn.expand '~'
+    --     .. '/files/notes/*/*.md',
+    --   -- 'BufNewFile ' .. vim.fn.expand '~' .. '/files/notes/*/*.md',
+    --   -- 'BufReadPre '
+    --   --   .. vim.fn.expand '~'
+    --   --   .. 'files/notes/*.md',
+    --   -- 'BufNewFile' .. vim.fn.expand '~' .. 'files/notes/*.md',
+    -- },
 
     dependencies = {
       -- Required.
@@ -37,7 +37,7 @@ return {
       -- see above for full list of optional dependencies ☝️
     },
     opts = {
-      dir = vim.env.HOME .. '/files/notes', -- specify the vault location. no need to call 'vim.fn.expand' here
+      dir = vim.fn.expand '~' .. '/files/notes', -- specify the vault location. no need to call 'vim.fn.expand' here
       use_advanced_uri = true,
       finder = 'telescope.nvim',
       log_level = vim.log.levels.DEBUG,
